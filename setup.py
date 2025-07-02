7e5aa7a6438488e5385067531b6007aa844d07e8
Ander Alvarez Sanz <aalvarezsanz@ideko.es>
2025-07-02 07:36:33 +0200

    .

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="idkDOE",
+    name="idkdoe",
     version="0.1.0",
     description="Librería de diseño de experimentos.",
     author="Ander Alvarez Sanz",
+    author_email='andersz.alvarez@gmail.com',  # Replace with your email
     packages=find_packages(where="src"),
     package_dir={"": "src"},
 )
